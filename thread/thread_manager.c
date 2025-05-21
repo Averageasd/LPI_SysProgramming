3b230c5b57a852f4519664c90756d87b8a2c2a96
Averageasd <nhuynh9497@gmail.com>
2025-05-21 01:53:23 +0000

    used heap data instead of main local vars

--- thread/thread_manager.c
+++ thread/thread_manager.c
@@ -16,14 +16,15 @@ static struct thread_object
 {
     pthread_t id;
     int status;
+    int thread_local_id;
 } *thread_array;
 
 static void *thread_work(void *);
 
 static void *thread_work(void *arg)
 {
-    printf("starting thread %lu\n", pthread_self());
     int local_tid = *((int *)arg);
+    printf("starting thread %d\n", local_tid);
     pthread_mutex_lock(&threadMutex);
     printf("thread %d is doing some work.\n", local_tid);
     printf("thread %d finishes\n", local_tid);
@@ -31,48 +32,43 @@ static void *thread_work(void *arg)
     thread_array[local_tid].status = 1;
     pthread_mutex_unlock(&threadMutex);
     pthread_cond_signal(&threadDied);
-    printf("signal parent\n");
+    printf("signal main thread\n");
     return NULL;
 }
 
 int main()
 {
-   
+
     int i;
     int s;
     int idx;
     int num_threads = NUM_TH;
-     thread_array = malloc(sizeof(*thread_array) * NUM_TH);
+    thread_array = malloc(sizeof(*thread_array) * NUM_TH);
     for (i = 0; i < NUM_TH; i++)
     {
         thread_array[i].status = 0;
-        pthread_create(&thread_array[i].id, NULL, thread_work, &i);
+        thread_array[i].thread_local_id = i;
+        pthread_create(&thread_array[i].id, NULL, thread_work, &thread_array[i].thread_local_id);
     }
-    while (thread_collected!=num_threads){
-        printf("starting main thread %ld\n", pthread_self());
+    while (thread_collected != num_threads)
+    {
         pthread_mutex_lock(&threadMutex);
-        printf("num thread done %d\n", thread_done);
-        while (thread_done == 0){
+        while (thread_done == 0)
+        {
             pthread_cond_wait(&threadDied, &threadMutex);
         }
-        for (idx = 0; idx<num_threads;idx++){
-            printf("thread %d and status of thread %d\n", idx, thread_array[idx].status);
-            if (thread_array[idx].status == 1){
-                s = pthread_join(thread_array[idx].id,NULL);
-                if (s!=0){
-                    printf("error joining thread %d\n", idx);
-                }
+        for (idx = 0; idx < num_threads; idx++)
+        {
+            if (thread_array[idx].status == 1)
+            {
+                s = pthread_join(thread_array[idx].id, NULL);
                 thread_array[idx].status = 2;
-                printf("thread done %d\n", thread_done);
-                printf("join thread %d\n", idx);
-                if (thread_done > 0){
+                printf("Reaped thread %d\n", thread_array[idx].thread_local_id);
                     thread_done--;
                     thread_collected++;
-                }
+                    printf("%d remaining threads.\n",(num_threads-thread_collected));
             }
         }
-        printf("number of collected threads %d\n", thread_collected);
-        printf("main thread unlocking thread mutex\n");
         pthread_mutex_unlock(&threadMutex);
     }
     return 0;
